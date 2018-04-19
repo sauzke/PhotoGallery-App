08e6aa64b8d183f7560557f75a1b87c34ecdc636
Jimmy Wang <jimmywang2142@gmail.com>
2018-04-18 20:55:38 -0700

    added logic to setting button with a new view

---
+++ app/src/main/java/com/example/a00914567/photogallery/DisplaySettingActivity.java
@@ -0,0 +1,21 @@
+package com.example.a00914567.photogallery;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DisplaySettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_setting);
+
+//        Intent intent = getIntent();
+//        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+//
+//        TextView textView = findViewById(R.id.text_simple);
+//        textView.setText(message);
+    }
+}
--- app/src/main/java/com/example/a00914567/photogallery/MainActivity.java
+++ app/src/main/java/com/example/a00914567/photogallery/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.a00914567.photogallery;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    //public static final String EXTRA_MESSAGE = "example text";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // When setting button is clicked
+    public void settingButton(View view){
+        // Sends user to the setting activity
+        Intent intent = new Intent(this, DisplaySettingActivity.class);
+//        EditText editText = (EditText) findViewById(R.id.editText);
+//        String message = editText.getText().toString();
+//        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
