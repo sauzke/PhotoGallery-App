d0516f241ee061433b112918e24e1b6b0e7c50b1
Jimmy Wang <A00914567@ad.bcit.ca>
2018-04-25 17:40:02 -0700

    adding features to save picture

--- app/src/main/java/com/example/a00914567/photogallery/MainActivity.java
+++ app/src/main/java/com/example/a00914567/photogallery/MainActivity.java
@@ -2,11 +2,19 @@ package com.example.a00914567.photogallery;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     static final int REQUEST_IMAGE_CAPTURE = 1;
@@ -22,7 +30,18 @@ public class MainActivity extends AppCompatActivity {
     public void takePicture(View view) {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if(takePictureIntent.resolveActivity(getPackageManager()) != null){
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            File photoFile = null;
+            try{
+                photoFile = createImageFile();
+            }catch(IOException e){
+                // Catch exception
+            }
+
+            if(photoFile != null) {
+                Uri photoUri = FileProvider.getUriForFile(this,"com.example.android.fileprovider",photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,photoUri);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
         }
     }
 
@@ -46,4 +65,15 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
 
     }
+
+    private File createImageFile() throws IOException{
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String filename = "PhotoGalleryImage_" + timeStamp + "_";
+        File storeDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(filename,".jpg",storeDir);
+        return image;
+    }
+
+
+
 }
