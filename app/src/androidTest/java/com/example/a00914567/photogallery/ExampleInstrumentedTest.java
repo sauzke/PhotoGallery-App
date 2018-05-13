9391ffe1b8d7cf11530d1e3f39a7f1ed17f0dd85
Jimmy <jimmywang2142@gmail.com>
2018-05-13 14:46:18 -0700

    Unit test for captions

--- app/src/androidTest/java/com/example/a00914567/photogallery/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/a00914567/photogallery/ExampleInstrumentedTest.java
@@ -1,13 +1,24 @@
 package com.example.a00914567.photogallery;
 
 import android.content.Context;
+import android.os.Environment;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+import android.text.style.TtsSpan;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.io.File;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
 import static org.junit.Assert.*;
+
+import static com.example.a00914567.photogallery.PhotoMange.FileManager.*;
+import static com.example.a00914567.photogallery.PhotoMange.PhotoDetailsManager.*;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -17,11 +28,24 @@ import static org.junit.Assert.*;
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
-    public void useAppContext() {
+    public void testCaptionStorage() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
+        File dir = appContext.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File fileDir = appContext.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS);
 
-        //assertEquals("com.example.a00914567.photogallery", appContext.getPackageName());
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+        try {
+            ArrayList<String> list = getPictureByDate(dir, dateFormat.parse("2018/05/01"),dateFormat.parse("2018/05/30"));
+            assertEquals(false,list.get(0).isEmpty());
+            String filePath = dir.getAbsolutePath() + "/" + list.get(0);
+
+            setComment(filePath,"Testing Comment",fileDir);
+
+            String comment = getComment(fileDir,dir,filePath);
+            assertEquals("Testing Comment", comment);
+        }catch(Exception e){
+        }
     }
 
 }
